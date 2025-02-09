6327567e9ceb0e2511f3ec1e5ec097f506846473
ajaykumar2004 <ajaykumar30802004@gmail.com>
2025-02-09 16:25:38 +0530

    pnc in rabbitq

---
+++ RabbitMQ/Producer/Producer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Producer
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            string message = "Hello, RabbitMQ!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                routingKey: "hello",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($"[x] Sent: {message}");
+        }
+    }
+}
